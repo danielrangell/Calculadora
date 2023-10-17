268ed461b88a2ccd2c5494c90b405387db0625f0
Daniel Rangel Aguiar <danielraguiar.17@gmail.com>
2023-10-17 15:14:46 -0300

    atualização layout e implementação JS

---
+++ calc.js
@@ -0,0 +1,22 @@
+function insert(num) {
+    var display = document.getElementById('display');
+    while (display.textContent.length < 15) {
+        document.getElementById('display').innerHTML += num;
+        break;
+    }
+}
+function clean() {
+    document.getElementById('display').innerHTML = "";
+}
+function back() {
+    var result = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = result.substring(0, result.length - 1);
+}
+function calcular() {
+    var result = document.getElementById('display').innerHTML;
+    if (result) {
+        document.getElementById('display').innerHTML = eval(result);
+    } else {
+        document.getElementById('display').innerHTML = 'nada...';
+    }
+}
