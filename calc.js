ed304e4d0be82347a6bbc908a2b26e788cd34f33
Daniel Rangel Aguiar <danielraguiar.17@gmail.com>
2023-10-30 17:41:18 -0300

    adicionando cometários no JS

--- calc.js
+++ calc.js
@@ -1,22 +1,33 @@
 function insert(num) {
-    var display = document.getElementById('display');
-    while (display.textContent.length < 15) {
-        document.getElementById('display').innerHTML += num;
-        break;
-    }
+  //insere numeros no display
+  var display = document.getElementById("display"); //pega o display
+  while (display.textContent.length < 15) {
+    //enquanto o display for menor que 15 caracteres
+    display.textContent += num; //adiciona o numero ao display
+    break; //para o loop
+  }
 }
 function clean() {
-    document.getElementById('display').innerHTML = "";
+  //limpa o display
+  document.getElementById("display").textContent = ""; //coloca o display vazio
 }
 function back() {
-    var result = document.getElementById('display').innerHTML;
-    document.getElementById('display').innerHTML = result.substring(0, result.length - 1);
+  //remove o ultimo caractere do display
+  var result = document.getElementById("display").textContent;
+  document.getElementById("display").textContent = result.substring(
+    //coloca o display com o ultimo caractere removido
+    0,
+    result.length - 1
+  );
 }
 function calcular() {
-    var result = document.getElementById('display').innerHTML;
-    if (result) {
-        document.getElementById('display').innerHTML = eval(result);
-    } else {
-        document.getElementById('display').innerHTML = 'ERRO';
-    }
+  //calcula o que esta no display
+  var result = document.getElementById("display").textContent;
+  if (result) {
+    //se o display não estiver vazio
+    document.getElementById("display").textContent = eval(result); //coloca no display o resultado da expressao
+  } else {
+    //se o display estiver vazio
+    document.getElementById("display").textContent = "ERRO"; //Mensagem caso de erro
+  }
 }
